55bffb08097f4a1bdcbfda88d7e358a77e5cff85
alpersalik <salika@mef.edu.tr>
2022-02-02 15:58:28 +0300

    Initial Commit

---
+++ Scanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scanner
+//
+//  Created by Alper Salık on 2.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Alper Salık on 2.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
